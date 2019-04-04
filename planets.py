416768665a9214eaeb8cd6b1e15c7f7485594096
Trilok Debbad <tdebbad@MacBook-Pro-2.local>
2019-04-03 23:57:12 -0700

    removed extra casting.

--- planets.py
+++ planets.py
@@ -1,10 +1,10 @@
 def weight_on_planets():
    w = int(input('What do you weigh on earth? '))
 
-   wmars =  str(0.38 * w)
-   wjup =  str(2.34 * w)
+   wmars =  0.38 * w
+   wjup =  2.34 * w
 
-   print('\nOn Mars you would weigh '+wmars+' pounds.\nOn Jupiter you would weigh '+wjup+' pounds.')
+   print('\nOn Mars you would weigh', wmars, 'pounds.\nOn Jupiter you would weigh', wjup, 'pounds.')
 
    
    
